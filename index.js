a5d1f2c99dabc86df4d90b5c340050beec01f1ee
Ajithvikram <pajithvikram.itprofession@gmail.com>
2020-07-27 21:09:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-module.exports = (str) => {
-    return `💩${str}💩`;
+module.exports = (array, size) => {
+    const newArray = [];
+    const arraySize = Math.round(array.length / size);
+    while (array.length) {
+        const chunk = array.splice(0, arraySize);
+        newArray.push(chunk);
+    }
+    return newArray;
 }
